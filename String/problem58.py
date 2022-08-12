499ba3e78600f0439a7b293a1d13ee2efc58edc8
Ricky Wu <rickywu@Rickys-MacBook-Air.local>
2022-08-11 23:20:04 -0600

    p58

---
+++ String/problem58.py
@@ -0,0 +1,49 @@
+"""
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+eg 1. 
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+eg. 2 
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+
+"""
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+ #need a counter for the last word
+        count = 0
+        
+        # remove all white spaces in sentence    
+        s = s.strip()
+        
+        # read string from end to beginning
+        start = len(s)-1
+        
+        #reading from right to left (backwards)
+        for i in range(start, -1, -1):
+            
+            #iterate until we find a space
+            if s[i] == ' ':
+                 break          
+            count += 1
+            
+        #else return out answer
+        return count
