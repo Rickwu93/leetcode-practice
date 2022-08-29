659c541e50ecfd57b2bc95ea444eb516a38d835a
Ricky Wu <rickywu209@gmail.com>
2022-08-28 19:30:17 -0600

    pb682

---
+++ Math/problem682.py
@@ -0,0 +1,61 @@
+"""
+You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x - Record a new score of x.
+"+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+"D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+"C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+Return the sum of all the scores on the record. The test cases are generated so that the answer fits in a 32-bit integer.
+
+
+eg.1
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+
+eg.2
+
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"-2" - Add -2 to the record, record is now [5, -2].
+"4" - Add 4 to the record, record is now [5, -2, 4].
+"C" - Invalidate and remove the previous score, record is now [5, -2].
+"D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+"9" - Add 9 to the record, record is now [5, -2, -4, 9].
+"+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+"+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+
+Constraints:
+
+1 <= ops.length <= 1000
+ops[i] is "C", "D", "+", or a string representing an integer in the range [-3 * 104, 3 * 104].
+For operation "+", there will always be at least two previous scores on the record.
+For operations "C" and "D", there will always be at least one previous score on the record.
+"""
+
+##bunch of elif and manipulating elements in the array, also need to change string to int
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        output=[]
+        for i in range(len(ops)):
+            if ops[i]=='C':
+                output.remove(output[-1])
+            elif ops[i]=='D':
+                output.append(2*int(output[-1]))
+            elif ops[i]=='+':
+                output.append(int(output[-1]+int(output[-2])))
+            else:
+                output.append(int(ops[i]))
+        return sum(output)
+            
