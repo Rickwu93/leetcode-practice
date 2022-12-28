3f0ed03b2454ebb86f681974cd52db36210a055e
Ricky Wu <rickywu209@gmail.com>
2022-12-27 21:07:25 -0700

    pb1768

---
+++ String/problem1768.js
@@ -0,0 +1,50 @@
+/*
+https://leetcode.com/problems/merge-strings-alternately/
+
+1768. Merge Strings Alternately
+Easy
+852
+17
+Companies
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+eg1. Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+eg2.
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+
+eg3.
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+*/
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+ var mergeAlternately = function (word1, word2) {
+    let output = [];
+  
+    for (i = 0; i < 100; i++) {
+      output.push(word1[i]), output.push(word2[i]);
+    }
+  
+    return output.join("");
+  };
